bb2ab3af1ad09a406521abd48d4f55f0a1bbbee0
Rafaella Ribeiro <rafaellaribeirop07@gmail.com>
2026-05-29 09:03:52 -0300

    comentários adicionados

--- ii-estruturas-condicionais/danoCritico/Program.cs
+++ ii-estruturas-condicionais/danoCritico/Program.cs
@@ -10,19 +10,22 @@
 
             Console.WriteLine("Jogue o dado e escreva o número: [1 a 20]");
             int dado = int.Parse(Console.ReadLine());
-
+            
             int danoTotal = 0;
 
             if (dado > 20 || dado < 1)
+            // Se o número do dado não estiver entre 1 e 20, exibe a mensagem de erro
             {
                 Console.WriteLine("Apenas números entre 1 e 20");
 
             } else if (dado == 20)
+            // Ataque crítico ocorre quando o número do dado é 20, multiplicando o dano base por 3
             {
                 Console.WriteLine("ATAQUE CRÍTICO");
                 danoTotal = danoBase * 3;
             }
             else
+            // Dano base se mantém para qualquer outro número do dado
             {
                 danoTotal = danoBase;
             }
