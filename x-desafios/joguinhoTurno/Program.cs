ba8f567936bfbd87e6b93fef5260bad3d6c1fc90
Rafaella Ribeiro <rafaellaribeirop07@gmail.com>
2026-06-02 21:56:08 -0300

    update: focous

--- x-desafios/joguinhoTurno/Program.cs
+++ x-desafios/joguinhoTurno/Program.cs
@@ -2,15 +2,16 @@
 {
     internal class Program
     {
+        static Random random = new Random();
         static void Main(string[] args)
         {
             //Atributos do personagem
-            int vidaPersonagem = 120;
-            int focousPersonagem = 1;
+            int vidaPersonagem = 150;
+            int focousPersonagem = 0;
             int danoBasePersonagem = 30;
 
             //Atributos do boss
-            int vidaBoss = 300;
+            int vidaBoss = 250;
             int danoBaseBoss = 20;
 
 
@@ -38,7 +39,15 @@
 
                         case 2:
                             //Ataque carregado: Causa dano igual ao dobro do dano base do personagem
-                            danoPersonagem = ataqueCarregado(danoBasePersonagem);
+                            if (focousPersonagem > 0)
+                            {
+                                danoPersonagem = ataqueCarregado(danoBasePersonagem);
+                                focousPersonagem--;
+                            } else
+                            {
+                                Console.WriteLine("Não há focous suficiente");
+                                danoPersonagem = ataqueBasico(danoBasePersonagem);
+                            }
                             break;
 
                         case 3:
@@ -53,9 +62,11 @@
                             break;
                     }
 
+                    Console.WriteLine("Você deu: " + danoPersonagem + " de dano.");
+                    Console.WriteLine();
                     vidaBoss -= danoPersonagem;
 
-                    hud(vidaPersonagem, vidaBoss);
+                    hud(vidaPersonagem, vidaBoss, focousPersonagem);
                     Console.WriteLine();
                 }
                
@@ -78,11 +89,13 @@
                         vidaPersonagem = 0;
                     }
 
-                    hud(vidaPersonagem, vidaBoss);
+                    hud(vidaPersonagem, vidaBoss, focousPersonagem);
                     Console.WriteLine();
                 }
              
                 turno++;
+                //A cada turno, o personagem ganha 1 focous
+                focousPersonagem++;
 
             } while (vidaPersonagem > 0 && vidaBoss > 0);
 
@@ -94,87 +107,73 @@
             else
             {
                 Console.WriteLine("Parabéns! Você derrotou o boss!");
-
             }
+        }
 
-            static void hud(int vidaP, int vidaB)
+        //======= MÉTODOS =======
+
+        //Mostrar HUD
+        static void hud(int vidaP, int vidaB, int focous)
+        {
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            Console.WriteLine("| Vida: " + vidaP + " | Focous: " + focous + " | Boss: " + vidaB + " |");
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        }
+
+        //Dano do boss: O boss tem uma chance de 50% de causar um ataque básico (dano igual ao dano base do boss) ou um ataque carregado (dano igual ao dobro do dano base do boss) 
+        static int calcularDanoBoss(int danoBase)
+        {
+            int habilidadeBoss = random.Next(1, 3); //Gera um número aleatório entre 1 e 2 para a habilidade do boss
+            int danoTotal;
+
+            if (habilidadeBoss == 1)
             {
-                Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-");
-                Console.WriteLine("| Vida: " + vidaP + " | Boss: " + vidaB + " |");
-                Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-");
+                Console.WriteLine("O boss deu um ataque básico");
+                danoTotal = danoBase;
             }
-
-            static int calcularDanoBoss(int danoBase)
+            else
             {
-                Random random = new Random();
-                int habilidadeBoss = random.Next(1, 3); //Gera um número aleatório entre 1 e 2 para a habilidade do boss
-                int danoTotal;
-
-                if (habilidadeBoss == 1)
-                {
-                    Console.WriteLine("O boss deu um ataque básico");
-                    danoTotal = danoBase;
-                }
-                else
-                {
-                    Console.WriteLine("O boss deu um ataque carregado");
-                    danoTotal = danoBase * 2;
-                }
-
-                return danoTotal;
+                Console.WriteLine("O boss deu um ataque carregado");
+                danoTotal = danoBase * 2;
             }
 
-            static int ataqueBasico(int danoBase)
+            return danoTotal;
+        }
+
+        static int ataqueBasico(int danoBase)
+        {
+            int danoTotal = danoBase;
+            return danoTotal;
+        }
+
+        static int ataqueCarregado(int danoBase)
+        {
+            int danoTotal = danoBase * 2;
+            return danoTotal;
+        }
+
+        //Poção de cura: Restaura 40 de vida
+        static int pocaoCura(int vida)
+        {
+            int vidaRestaurada = vida + 40;
+            //Não pode ultrapassar a vida máxima do personagem (120)
+            if (vidaRestaurada > 120)
             {
-                int danoTotal = danoBase;
-                return danoTotal;
+                vidaRestaurada = 120;
             }
+            return vidaRestaurada;
+        }
 
-            static int ataqueCarregado(int danoBase)
-            {
-                int danoTotal = danoBase * 2;
-                return danoTotal;
-            }
+        //Verificar se o boss está vivo (Boss não pode atacar se tiver sido derrotado)
+        static bool bossVivo(int vidaBoss)
+        {
+            return vidaBoss > 0;
+        }
 
-            static int pocaoCura(int vida)
-            {
-                int vidaRestaurada = vida + 40;
-                if (vidaRestaurada > 120)
-                {
-                    vidaRestaurada = 120;
-                }
-                return vidaRestaurada;
-            }
-
-            static bool bossVivo(int vidaBoss)
-            {
-                bool vivo;
-                if (vidaBoss > 0)
-                {
-                    vivo = true;
-                }
-                else
-                {
-                    vivo = false;
-                }
-                return vivo;
-            }
-
-            static bool personagemVivo(int vidaPersonagem)
-            {
-                bool vivo;
-                if (vidaPersonagem > 0)
-                {
-                    vivo = true;
-                }
-                else
-                {
-                    vivo = false;
-                }
-                return vivo;
-
-
-            }
+        //Verificar se o personagem está vivo (Personagem não pode atacar se tiver sido derrotado)
+        static bool personagemVivo(int vidaPersonagem)
+        {
+            return vidaPersonagem > 0;
         }
     }
 }
